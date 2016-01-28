f31d96fc1f88b56908011c83269166d3b2586914
Ester Lorente <ester.lorente@outlook.com>
2016-01-28 19:06:59 +0100

    Button pressed

--- app/src/main/java/com/esterlorente/jediapp/CalculadoraActivity.java
+++ app/src/main/java/com/esterlorente/jediapp/CalculadoraActivity.java
@@ -2,6 +2,10 @@ package com.esterlorente.jediapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +18,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
@@ -51,41 +56,7 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
             textOper.setText(value);
         } else Log.e(TAG, "Intent - No hay valor");
 
-        button0 = (Button) findViewById(R.id.button_0);
-        button1 = (Button) findViewById(R.id.button_1);
-        button2 = (Button) findViewById(R.id.button_2);
-        button3 = (Button) findViewById(R.id.button_3);
-        button4 = (Button) findViewById(R.id.button_4);
-        button5 = (Button) findViewById(R.id.button_5);
-        button6 = (Button) findViewById(R.id.button_6);
-        button7 = (Button) findViewById(R.id.button_7);
-        button8 = (Button) findViewById(R.id.button_8);
-        button9 = (Button) findViewById(R.id.button_9);
-        buttonPlus = (Button) findViewById(R.id.button_plus);
-        buttonMin = (Button) findViewById(R.id.button_min);
-        buttonProd = (Button) findViewById(R.id.button_prod);
-        buttonDiv = (Button) findViewById(R.id.button_div);
-        buttonDot = (Button) findViewById(R.id.button_dot);
-        buttonEqu = (Button) findViewById(R.id.button_equ);
-        buttonDel = (Button) findViewById(R.id.button_del);
-
-        button0.setOnClickListener(this);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-        button8.setOnClickListener(this);
-        button9.setOnClickListener(this);
-        buttonPlus.setOnClickListener(this);
-        buttonMin.setOnClickListener(this);
-        buttonProd.setOnClickListener(this);
-        buttonDiv.setOnClickListener(this);
-        buttonDot.setOnClickListener(this);
-        buttonEqu.setOnClickListener(this);
-        buttonDel.setOnClickListener(this);
+        initButtons();
     }
 
     @Override
@@ -149,6 +120,7 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View v) {
         String text = textOper.getText().toString();
+
         switch (v.getId()) {
             case R.id.button_0:
                 textOper.setText(text + "0");
@@ -198,6 +170,9 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
             case R.id.button_del:
                 if (text.length() > 0)
                     textOper.setText(text.subSequence(0, text.length() - 1));
+                else {
+                    //TODO: toast
+                }
                 break;
         }
 
@@ -220,6 +195,7 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
                 break;
             case R.id.button_equ:
                 result = evaluateExpression(textOper.getText().toString());
+
 
                 Animation animOper = AnimationUtils.loadAnimation(this, R.anim.calc_oper);
                 animOper.reset();
@@ -261,17 +237,74 @@ public class CalculadoraActivity extends AppCompatActivity implements View.OnCli
                 });
                 textRes.startAnimation(animRes);
 
+
+
+
                 /*
-                AnimationSet animation = new AnimationSet(false);
-                Animation  moveDownUp = new TranslateAnimation(0, 0, 0, -textRes.getHeight());
-                moveDownUp.setDuration(700);
-                animation.addAnimation(moveDownUp);
-                animation.addAnimation(animRes);
+                AnimationSet animation = new AnimationSet(true);
+
+
+                float fromXposition = textOper.getX();
+                float toXPosition = textOper.getX();
+                float fromYPosition = textOper.getY();
+                float toYPosition = textOper.getX()-textOper.getHeight();
+                TranslateAnimation transRes = new TranslateAnimation(fromXposition, toXPosition, fromYPosition, toYPosition);
+                transRes.setDuration(700);
+                animation.addAnimation(transRes);
+
+
+                float fromXscale = 1.0f;
+                float toXscale = 2.0f;
+                float fromYscale = 1.0f;
+                float toYscale = 2.0f;
+                ScaleAnimation scaleRes = new ScaleAnimation(fromXscale, toXscale, fromYscale, toYscale);
+                scaleRes.setDuration(700);
+                animation.addAnimation(scaleRes);
+
                 textRes.startAnimation(animation);
                 */
 
+
                 break;
         }
+    }
+
+    private void initButtons() {
+        button0 = (Button) findViewById(R.id.button_0);
+        button1 = (Button) findViewById(R.id.button_1);
+        button2 = (Button) findViewById(R.id.button_2);
+        button3 = (Button) findViewById(R.id.button_3);
+        button4 = (Button) findViewById(R.id.button_4);
+        button5 = (Button) findViewById(R.id.button_5);
+        button6 = (Button) findViewById(R.id.button_6);
+        button7 = (Button) findViewById(R.id.button_7);
+        button8 = (Button) findViewById(R.id.button_8);
+        button9 = (Button) findViewById(R.id.button_9);
+        buttonPlus = (Button) findViewById(R.id.button_plus);
+        buttonMin = (Button) findViewById(R.id.button_min);
+        buttonProd = (Button) findViewById(R.id.button_prod);
+        buttonDiv = (Button) findViewById(R.id.button_div);
+        buttonDot = (Button) findViewById(R.id.button_dot);
+        buttonEqu = (Button) findViewById(R.id.button_equ);
+        buttonDel = (Button) findViewById(R.id.button_del);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonMin.setOnClickListener(this);
+        buttonProd.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+        buttonDot.setOnClickListener(this);
+        buttonEqu.setOnClickListener(this);
+        buttonDel.setOnClickListener(this);
     }
 
     private String evaluateExpression(String text) {
